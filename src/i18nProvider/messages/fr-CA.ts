0b533cd2292541760dd00711fb159012280a79d4
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-04-30 19:31:51 +0100

    fix: typo

--- src/i18nProvider/messages/fr-CA.ts
+++ src/i18nProvider/messages/fr-CA.ts
@@ -3,7 +3,7 @@ import { LOCALES } from '../constants';
 export default {
     [LOCALES.FRENCH]: {
         'hello': 'Bonjour',
-        'edit-file': 'Modifiez {fichier} et enregistrez pour recharger.',
+        'edit-file': 'Modifiez {file} et enregistrez pour recharger.',
         'placeholder': 'Échantillon espace réservé',
     },
 };
